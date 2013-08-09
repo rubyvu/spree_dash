09ec0c617ab5a935d84ae940350ca5d3bd9838f2
Ruby Vu <rubyvu.vn@gmail.com>
2013-08-09 13:33:24 +0700

    first commit

---
+++ lib/spree/dash.rb
@@ -0,0 +1,15 @@
+require 'spree_backend'
+require 'httparty'
+
+module Spree
+  module Dash
+
+  end
+end
+
+require 'spree/dash/engine'
+require 'spree/dash/jirafe'
+
+Spree::Dash::Engine.config.to_prepare do
+  Spree::BaseController.send :helper, 'spree/analytics'
+end
